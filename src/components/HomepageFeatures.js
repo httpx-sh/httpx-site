0d73d7b702cc1236404a3ff3eeaa83a937cd8f39
linux_china <libing.chen@gmail.com>
2022-01-30 14:11:22 -0800

    initial version

---
+++ src/components/HomepageFeatures.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './HomepageFeatures.module.css';
+
+const FeatureList = [
+    {
+        title: 'Easy to Use',
+        Svg: require('../../static/img/httpx-cli.svg').default,
+        description: require('../../static/img/httpx-shebang.png').default,
+    },
+    {
+        title: 'HTTP Support',
+        Svg: require('../../static/img/http-logo.svg').default,
+        description: require('../../static/img/http-post.png').default,
+    },
+    {
+        title: 'gRPC support',
+        Svg: require('../../static/img/grpc-logo.svg').default,
+        description: require('../../static/img/grpc-sayhello.png').default,
+    },
+    {
+        title: 'RSocket',
+        Svg: require('../../static/img/rsocket-logo.svg').default,
+        description: require('../../static/img/rsocket-request.png').default,
+    },
+    {
+        title: 'GraphQL',
+        Svg: require('../../static/img/graphql-logo.svg').default,
+        description: require('../../static/img/graphql-over-ws.png').default,
+    },
+    {
+        title: 'Misc',
+        Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+        description: require('../../static/img/shell-completion.png').default,
+    },
+];
+
+function Feature({Svg, title, description}) {
+    return (
+        <div className={clsx('col col--4')}>
+            <div className="text--center">
+                <Svg className={styles.featureSvg} alt={title}/>
+            </div>
+            <div className="text--center padding-horiz--md">
+                <h3>{title}</h3>
+                <img src={description}/>
+            </div>
+        </div>
+    );
+}
+
+export default function HomepageFeatures() {
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+}
